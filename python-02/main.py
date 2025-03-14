266c2c4eba2c9127f3381f16d5aac576b658503d
rafaeIf <rafa.fernandes16052007@gmail.com>
2025-03-14 19:45:23 -0300

    operadores (atualizado)

--- python-02/main.py
+++ python-02/main.py
@@ -1,9 +1,15 @@
 x = 10
-y = x - 5 * 2
+y = x + 5
 
 divisao = 10 / 2
 divisao_inteira = 10 // 2 # não aproxima, só mostra a divisão inteira
 modulo = 10 % 2 # resto da divisão
+potencia = 5 ** 2
 
 print(x)
 print(y)
+
+print(divisao)
+print(divisao_inteira)
+print(modulo)
+print(potencia)
