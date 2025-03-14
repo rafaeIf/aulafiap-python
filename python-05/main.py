ca9c57c842bfe5201b942708cc0921880b73d0ab
rafaeIf <rafa.fernandes16052007@gmail.com>
2025-03-14 20:57:28 -0300

    verificações em bool

---
+++ python-05/main.py
@@ -0,0 +1,13 @@
+10 == 10 # True
+10 == 11 # False
+
+10 == 10 or 10 == 11 # True
+10 == 10 and 10 == 11 # False
+
+True and True # True
+True and False # True
+
+True ^ False # True
+True ^ True # False
+
+print(True ^ True)
