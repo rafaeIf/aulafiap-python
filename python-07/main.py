b6fbdcef3e3e826739662db6d8c62a47e7e05783
rafaeIf <rafa.fernandes16052007@gmail.com>
2025-03-14 20:59:31 -0300

    coversões e functions

---
+++ python-07/main.py
@@ -0,0 +1,14 @@
+nome = 'rafael'
+valor = 38
+
+print(
+str(1000), 
+int(39), 
+float(39), 
+bool(' '), #espaço conta como caractere (True)
+int(3.93836),
+int(True),
+int(False),
+nome.isdigit(),
+str(valor).isdigit() # tem q ser string para funcionar
+)
