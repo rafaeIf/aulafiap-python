8c0e0e2acd21be401108d2d5714a16e700a76098
rafaeIf <rafa.fernandes16052007@gmail.com>
2025-03-25 23:54:08 -0300

    laço for

---
+++ python-12/main.py
@@ -0,0 +1,16 @@
+lista = [1, 3, 5]
+
+for i in lista:
+    print (i)
+
+for i in range(0, 11, 5): # (onde começa, enquanto for < x, quantos q vai pular)
+    print(i)
+
+dicionario = { # key, value (k, v)
+    'segunda':1,
+    'terça':2,
+    'quarta':3
+}
+
+for i in dicionario.items():
+    print(i)
